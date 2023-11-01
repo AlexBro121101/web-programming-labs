8f42dd85718d93196ddc6d8a57f724b7ab050136
Александр Бодрых <webproga121101@gmail.com>
2023-11-01 11:23:35 +0700

    Меню

--- app.py
+++ app.py
@@ -2,8 +2,39 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
+@app.route("/index")
 def start():
     return """
+<!doctype html>
+<html>
+    <head>
+        <title>НГТУ, ФБ, Лабораторные работы</title>
+    </head>
+
+    <body>
+        <header>
+            НГТУ, ФБ, WEB-программирование, часть 2. Список лабораторных
+        </header>   
+
+        <main>
+            <ol>
+                <li>
+                    <a href="/lab1">Лабораторнгая работа 1</a>
+                </li>
+            </ol?
+        </main>
+
+        <footer>
+            &copy; Бодрых Александр Юрьевич, ФБИ-14, 3 курс, 2023
+        </footer>
+    </body>
+</html>
+"""
+
+@app.route("/lab1")
+def lab1():
+    return """
+
 <!doctype html>
 <html>
     <head>
@@ -17,10 +48,21 @@ def start():
 
         <h1> web-сервер на flask</h1>
 
+        <div>
+            Flask — фреймворк для создания веб-приложений на языке
+            программирования Python, использующий набор инструментов
+            Werkzeug, а также шаблонизатор Jinja2. Относится к категории так
+            называемых микрофреймворков — минималистичных каркасов
+
+            веб-приложений, сознательно предоставляющих лишь самые ба-
+            зовые возможности.
+        </div>
+
+       
+
         <footer>
             &copy; Бодрых Александр, ФБИ-14, 3 курс, 2023
         </footer>
     </body>
 </html>
 """
-
