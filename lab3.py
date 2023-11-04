0e883d83594b77f79817cd8ec01a759234355acc
Александр Бодрых <webproga121101@gmail.com>
2023-11-04 19:55:15 +0700

    Форма

--- lab3.py
+++ lab3.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 lab3 = Blueprint('lab3',__name__)
 
 
@@ -8,4 +8,7 @@ def lab():
 
 @lab3.route('/lab3/form1')
 def form1():
-    return render_template('form1.html')
+    user = request.args.get('user')
+    age = request.args.get('age')
+    sex = request.args.get('sex')
+    return render_template('form1.html', user=user, age=age, sex=sex)
