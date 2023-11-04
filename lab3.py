078475714a182bbd4086233f4e397719648a0f73
Александр Бодрых <webproga121101@gmail.com>
2023-11-04 19:21:55 +0700

    Заготовка к третьей лабораторной работе

---
+++ lab3.py
@@ -0,0 +1,11 @@
+from flask import Blueprint, render_template
+lab3 = Blueprint('lab3',__name__)
+
+
+@lab3.route('/lab3/')
+def lab():
+    return render_template('lab3.html')
+
+@lab3.route('/lab3/form1')
+def form1():
+    return render_template('form1.html')
